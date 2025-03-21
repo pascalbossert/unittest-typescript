6f889acbf982d42b6d5cb1e791ee0da0eacaead2
Pascal Bossert <pascal.bossert@kauz.ch>
2025-03-21 08:53:05 +0100

    Aufgabe 2.2: Fehler in Funktion einbauen und testen

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,7 @@
 export function add(summandA: number, summandB: number): number {
-  return summandA + summandB;
+  return summandA + summandB + 1; // absichtlicher Fehler eingebaut
 }
+
 
 export function subtract(minuend: number, subtrahend: number): number {
   return minuend - subtrahend;
