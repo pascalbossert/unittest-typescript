9752c61e08066c165e16400c613e2022421667bc
Pascal Bossert <pascal.bossert@kauz.ch>
2025-03-21 08:54:30 +0100

    Aufgabe 2.2: Revert Changes

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,7 +1,6 @@
 export function add(summandA: number, summandB: number): number {
-  return summandA + summandB + 1; // absichtlicher Fehler eingebaut
+  return summandA + summandB;
 }
-
 
 export function subtract(minuend: number, subtrahend: number): number {
   return minuend - subtrahend;
