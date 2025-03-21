15dca89131d1835f714a6a3429325773e5bf79a5
Pascal Bossert <pascal.bossert@kauz.ch>
2025-03-21 09:20:15 +0100

    Aufgabe 6.3: Testdefinition Median Werte hinzugefügt und expected angepasst

--- src/median.test.ts
+++ src/median.test.ts
@@ -1,28 +1,29 @@
 import { median } from "./median";
 
 test("median for odd number of elements", () => {
-  // Arrange
-  const numbers: number[] = [1, 2, 6, 7, 8];
-  const expected: number = 6;
-
-  // Act
-  const actual: number = median(numbers);
-
-  // Assert
-  expect(actual).toBe(expected);
-});
-
-test("median for even number of elements", () => {
-  // Arrange
-  const numbers: number[] = [1, 2, 5, 6, 7, 8];
-  const expected: number = (5 + 6) / 2;
-
-  // Act
-  const actual: number = median(numbers);
-
-  // Assert
-  expect(actual).toBe(expected);
-});
+    // Arrange
+    const numbers: number[] = [1, 2, 6, 7, 8, 10, 12]; // Geändert von 5 auf 7 Zahlen
+    const expected: number = 7; // Falscher Wert, Test schlägt fehl
+  
+    // Act
+    const actual: number = median(numbers);
+  
+    // Assert
+    expect(actual).toBe(expected);
+  });
+  
+  test("median for even number of elements", () => {
+    // Arrange
+    const numbers: number[] = [1, 2, 5, 6, 7, 8, 9, 10]; // Geändert von 6 auf 8 Zahlen
+    const expected: number = (6 + 7) / 2; // Falscher Wert, Test schlägt fehl
+    
+    // Act
+    const actual: number = median(numbers);
+  
+    // Assert
+    expect(actual).toBe(expected);
+  });
+  
 
 test("median for no elements", () => {
   // Arrange
