538eca6609a4066f2f255878f7923ac4d8616b92
Pascal Bossert <pascal.bossert@kauz.ch>
2025-03-21 08:58:43 +0100

    Aufgabe 2.3: Testfall ändern, 'expected' anpassen damit Test passt.

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -2,9 +2,10 @@ import { add, subtract } from "./calculator";
 
 test("add three to two is five", () => {
   // Arrange
-  const a: number = 2;
+  const a: number = 4; // Geändert von 2 auf 4
   const b: number = 3;
-  const expected: number = 5;
+  const expected: number = 7; // Erwartungswert angepasst
+
 
   // Act
   const actual = add(a, b);
@@ -12,6 +13,7 @@ test("add three to two is five", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
 
 test("subtract four from six is two", () => {
   // Arrange
